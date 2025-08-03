18e9e0c70d6af2e0e239e084a13f78c0adaa662c
Juan Moreno <user_tekton29@skyairline.com>
2025-08-02 21:52:34 -0500

    vue template init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <h1>Hola Vue + Sass 👋</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.app {
+  h1 {
+    color: rebeccapurple;
+    font-weight: bold;
+  }
+}
+</style>
